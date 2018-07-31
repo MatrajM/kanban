dad33e2f07c730b7aab69fe8b4bde89071d3e39d
Michał Matraj <matraj.michal@op.pl>
2018-07-31 16:56:09 +0200

    kanban v2 pt1

---
+++ App.js
@@ -0,0 +1,46 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3415',
+  'X-Auth-Token': '9931a38bd8784d572df74f1a6b1bf855',
+  'Content-Type': 'application/json; charset=utf-8'
+};
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+/*
+function randomString() {
+	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
+	var str = '', i;
+	for (i = 0; i < 10; i++) {
+	  str += chars[Math.floor(Math.random() * chars.length)];
+	}
+	return str;
+}
+*/
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
+
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+          var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+  }
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
